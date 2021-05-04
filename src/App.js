ce580db3fa9caf78d315d55b7a73b32fcdb655c4
Chip <chippenzie@users.noreply.github.com>
2021-05-03 23:09:26 -0400

    multi-filter working

--- src/App.js
+++ src/App.js
@@ -31,24 +31,56 @@ class RemoteControl extends React.Component {
     this.state = {
       filters: {
         station: null,
+        location: null,
+        decade: null,
+        genre: null
       }
     }
     this.filterByStation = this.filterByStation.bind(this);
+    this.filterByLocation = this.filterByLocation.bind(this);
+    this.filterByDecade = this.filterByDecade.bind(this);
+    this.filterByGenre = this.filterByGenre.bind(this);
   }
 
   filterByStation(v) {
-    console.log(v.target.value);
     let currentFilters = this.state.filters;
     currentFilters.station = v.target.value;
     this.setState({
       filters: currentFilters
     });
   }
+  
+  filterByLocation(v) {
+    let currentFilters = this.state.filters;
+    currentFilters.location = v.target.value;
+    this.setState({
+      filters: currentFilters
+    });
+  }
+
+  filterByDecade(v) {
+    let currentFilters = this.state.filters;
+    currentFilters.decade = v.target.value;
+    this.setState({
+      filters: currentFilters
+    });
+  }
+
+  filterByGenre(v) {
+    let currentFilters = this.state.filters;
+    currentFilters.genre = v.target.value;
+    this.setState({
+      filters: currentFilters
+    });
+  }
 
   render() {
-    let streems = '',
-        stations = {},
-        currentFilters = this.state.filters;
+    let stations = {},
+        locations = {},
+        decades = {},
+        genres = {},
+        currentFilters = this.state.filters,
+        streems = 'ppp';
 
     if (this.props.streams.length) {
       for (let i=0; i < this.props.streams.length; i++) {
@@ -58,22 +90,66 @@ class RemoteControl extends React.Component {
         } else {
           stations[st.station] = 1
         }
+        if (locations[st.location]) {
+          locations[st.location]++;
+        } else {
+          locations[st.location] = 1
+        }
+        if (decades[st.decade]) {
+          decades[st.decade]++;
+        } else {
+          decades[st.decade] = 1
+        }
+        if (genres[st.genre]) {
+          genres[st.genre]++;
+        } else {
+          genres[st.genre] = 1
+        }
       }
 
-      streems = this.props.streams.map((st) => {
-        if (st.url) {
-          if ((currentFilters.station) && (st.station === currentFilters.station)) {
-            const title = st.title ? st.title : st.station + ' ' + st.location + ' ' + st.airdate;
-            return (<option value={st.url} key={st.url} >{title}</option>);
-          }        
-          
+      let filters = ['station', 'location', 'decade', 'genre'],
+        filterUsed = false;
+      streems = this.props.streams;
+
+      for (let i=0; i<filters.length; i++) {
+        //console.log('check ', filters[i]);
+        if (currentFilters[filters[i]]) {
+          filterUsed = true;
+          streems = streems.map((st) => {
+            if ((st) && (st.url)) {
+              if ((st[filters[i]] === currentFilters[filters[i]])) {
+                //console.log('ass', st);
+                //const title = st.title ? st.title : st.station + ' ' + st.location + ' ' + st.airdate;
+                //return (<option value={st.url} key={st.url} >{title}</option>);
+                return st
+              }        
+            }
+          });
         }
-      });
+      }
+
+      if (filterUsed) {
+        streems = streems.map((st) => {
+          if ((st) && (st.url)) {
+            const title = st.title ? st.title : st.station + ' ' + st.location + ' ' + st.airdate;
+
+            return (<option value={st.url} key={st.url} >{title}</option>);                    
+          } else {
+            return null;
+          }
+        });     
+      } else {
+        streems = [];
+      }
+
     }
 
     return (
       <div>
         <Filter id="station" label="Station" values={stations} filterFn={this.filterByStation} />
+        <Filter id="location" label="Locations" values={locations} filterFn={this.filterByLocation} />
+        <Filter id="decades" label="Decade" values={decades} filterFn={this.filterByDecade } />
+        <Filter id="genres" label="Genre" values={genres} filterFn={this.filterByGenre} />
         <label htmlFor="streamDropdown">Streams</label>
         <select id="streamDropdown">
             {streems}                
